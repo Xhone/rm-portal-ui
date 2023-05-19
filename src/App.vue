7f5de0d1b765ea5b58e281eac69788b71d5ee484
Xhone <659219179@qq.com>
2023-05-19 10:29:45 +0800

    update .gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo-M.gif">
+  <div
+  class="static"
+  :class="classObject"></div>
+  
+  <div>
+    <form>
+      <div>
+        <label>username:</label>
+      <input type="text" />
+      </div>
+      <div>
+        <label>password:</label>
+      <input type="password" />
+      </div>
+    
+      <ul>
+     <li v-for="q in quote" :key="q">{{ q }}</li>
+    </ul>
+    </form>
+   
+  </div>
+  <HelloWorld  msg="Wellcome to OMPortal"/>
+ 
 </template>
-
-<script>
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import {ref,reactive} from 'vue'
+const quote=ref('')
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const classObject=reactive({
+  active:true,
+  'text-danger':false
+})
+axios.get('https://localhost:44387/api/Student/1').then(reponse=>{
+      quote.value=reponse.data
+    }).catch(error=>{
+      console.log('error is '+error)
+    })   
 </script>
 
 <style>
