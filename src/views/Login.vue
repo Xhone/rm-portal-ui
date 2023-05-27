27834fae404085b43681d0122b21f55d55e82c5b
Xhone <659219179@qq.com>
2023-05-27 12:11:32 +0800

    add http modules

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,40 @@
 <template>
-    <div class="page">
-        <h2>Login Page</h2>
-    </div>
+   <el-form>
+
+   </el-form>>
 </template>
 
 <script>
-
+import { dataType } from "element-plus/es/components/table-v2/src/common";
+import Cookies from"js-cookie"
 export default {
-    name:'Login'  
+    name:'Login',
+    data(){
+        return{
+            logining:false,
+            loginForm:{
+                account:'admin',
+                password:'123456'
+            },
+            fieldRules:{
+                account:{
+                    required:true,
+                    message:'please enter your account',
+                    trigger:'blur'
+                },
+                password:{
+                    required:true,
+                    message:'please enter your password',
+                    trigger:'blur'
+                }
+            },
+            checked:true
+        }
+    },
+    methods:{
+        login(){
+            
+        }
+    }
 }
 </script>
